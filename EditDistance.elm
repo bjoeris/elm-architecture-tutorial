65efa3159bebd75e27e03246b03e2750122b6eb6
Benson Joeris <bjoeris@gmail.com>
2017-06-14 07:11:15 -0400

    Working on edit distance

--- EditDistance.elm
+++ EditDistance.elm
@@ -9,6 +9,7 @@ type ErrorOp
   | Left
   | Right
 
+
 score_matrix : Array String -> Array String -> Array (Array (Int, ErrorOp))
 score_matrix x y =
   let nx = Array.length x
@@ -24,9 +25,9 @@ score_matrix x y =
       grow i j rows rowCurr =
         case (Array.get i x, Array.get j y) of
           (Nothing, _) ->
-            Array.fromList (List.reverse rows)
+            Array.fromList rows
           (_, Nothing) ->
-            grow (i+1) 0 ((rowCurr |> List.reverse |> Array.fromList) :: rows) []
+            grow (i-1) (ny-1) ((rowCurr |> Array.fromList) :: rows) []
           (Just xi, Just yj) ->
             let stepi = List.head rows
                       |> Maybe.andThen (Array.get i)
@@ -36,13 +37,13 @@ score_matrix x y =
                 mismatch_cost = if xi == yj then 0 else 1
                 mismatch_op = if xi == yj then Match else Substitution
                 stepij = List.head rows
-                       |> Maybe.andThen (Array.get (i-1))
+                       |> Maybe.andThen (Array.get (i+1))
                        |> Maybe.map (\(k,_) -> k)
-                       |> withDefault 0
+                       |> withDefault (nx + ny - i - j + 2)
                        |> (\k -> (k+mismatch_cost, mismatch_op))
                 next = stepi |> minMaybe stepj |> minMaybe (Just stepij) |> withDefault stepij
-            in grow i (j+1) rows (next :: rowCurr)
-  in grow 0 0 [] []
+            in grow i (j-1) rows (next :: rowCurr)
+  in grow (nx-1) (ny-1) [] []
 
 score_sequence : Array (Array (Int, ErrorOp)) -> (Array Bool, Array Bool)
 score_sequence matrix =
@@ -52,28 +53,28 @@ score_sequence matrix =
         let getNextError op =
               case op of
                 Match ->
-                  getErrors (i-1) (j-1) (False :: xErrors) (False :: yErrors)
+                  getErrors (i+1) (j+1) (False :: xErrors) (False :: yErrors)
                 Substitution ->
-                  getErrors (i-1) (j-1) (True :: xErrors) (True :: yErrors)
+                  getErrors (i+1) (j+1) (True :: xErrors) (True :: yErrors)
                 Left ->
-                  getErrors (i-1) j (True :: xErrors) yErrors
+                  getErrors (i+1) j (True :: xErrors) yErrors
                 Right ->
-                  getErrors i (j-1) xErrors (True :: yErrors)
+                  getErrors i (j+1) xErrors (True :: yErrors)
             finalizeErrors n errorList =
-              let errorArray = Array.fromList errorList
+              let errorArray = Array.fromList (List.reverse errorList)
               in Array.append
-                  (Array.repeat (n - Array.length errorArray) True)
                   errorArray
+                  (Array.repeat (n - Array.length errorArray) True)
         in matrix |> Array.get i
                   |> Maybe.andThen (Array.get j)
                   |> Maybe.map (\(_, op) -> getNextError op)
                   |> withDefault (finalizeErrors nx xErrors,
                                   finalizeErrors ny yErrors)
-  in getErrors (nx-1) (ny-1) [] []
+  in getErrors 0 0 [] []
 
 
-reverse : Array a -> Array a
-reverse arr = arr |> Array.toList |> List.reverse |> Array.fromList
+countTrue : Array Bool -> Int
+countTrue = Array.foldl (\x n -> if x then (n+1) else n) 0
 
 score : Array String -> Array String -> { score : Int
                                         , leftErrors : Array Bool
@@ -82,16 +83,16 @@ score : Array String -> Array String -> { score : Int
 score x y =
   let nx = Array.length x
       ny = Array.length y
-      matrix = score_matrix (reverse x) (reverse y)
-      score = matrix
-            |> Array.get (nx-1)
-            |> Maybe.andThen (Array.get (ny-1))
-            |> Maybe.map (\(k,_) -> k)
-            |> withDefault 0
+      matrix = score_matrix x y
+      -- score = matrix
+      --       |> Array.get (nx-1)
+      --       |> Maybe.andThen (Array.get (ny-1))
+      --       |> Maybe.map (\(k,_) -> k)
+      --       |> withDefault 0
       errors = score_sequence matrix
                |> \(xErrors, yErrors) ->
-                   { score=score
-                   , leftErrors=(reverse xErrors)
-                   , rightErrors=(reverse yErrors)
+                   { score = countTrue yErrors
+                   , leftErrors=xErrors
+                   , rightErrors=yErrors
                    , matrix=matrix }
   in errors
